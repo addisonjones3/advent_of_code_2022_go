4ab08fed4d5798c98eac9ab29096b01e727ba26a
Addison <addison.jones3@gmail.com>
2022-12-16 21:46:38 -0500

    Day 12 started

--- challenge/day12/day12_test.go
+++ challenge/day12/day12_test.go
@@ -2,11 +2,35 @@ package day12
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestRange(t *testing.T) {
+	i := 27
 	for c := 'a'; c <= 'z'; c++ {
-		fmt.Println(c)
+		fmt.Println(string(c))
+		i--
 	}
+	fmt.Println('a' > 'c')
+	fmt.Println(string('a' + 1))
+}
+
+func TestNewRuneGridFromLines(t *testing.T) {
+	lines := strings.Split(sampleMap, "\n")
+	mg := NewMapGrid(lines)
+	for _, row := range mg.Grid {
+		nVals := make([]string, 0)
+		for _, n := range row {
+			nVals = append(nVals, string(n.Val))
+		}
+		fmt.Println(nVals)
+		fmt.Println()
+	}
+	n := mg.GetNode(Coords{X: 1, Y: 1})
+	fmt.Println(n.ValStr())
+}
+
+func TestMod(t *testing.T) {
+	fmt.Println(10%2 == 0)
 }
